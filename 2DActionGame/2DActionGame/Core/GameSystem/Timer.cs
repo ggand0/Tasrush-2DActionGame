8146263ce0df6d9d30cb57809e2d5f305f640132
faintmocha <faintmocha@gmail.com>
2012-03-06 15:48:41 +0900

    Endingのズレが治った・・・かな？
    Timer追加

---
+++ 2DActionGame/2DActionGame/Core/GameSystem/Timer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace _2DActionGame
+{
+    public class Timer
+    {
+        public Stopwatch sw = new Stopwatch();
+        public bool IsStarted { get { return sw.IsRunning; } }
+        public double CurrentTime { get { return (double)sw.ElapsedTicks / (double)Stopwatch.Frequency; } }
+        public double PrevTime { get; set; }
+        public long Tick { get { return sw.ElapsedTicks; } }
+
+        public void Start()
+        {
+            sw.Start();
+        }
+        public void Update()
+        {
+            PrevTime = CurrentTime;
+        }
+
+        /// <summary>
+        /// CurrentTime - PrevTimeによる１フレーム分の経過時間を測定します。実行した次の行でUpdateしてください。
+        /// </summary>
+        public double FrameTime()
+        {
+            return CurrentTime - PrevTime;
+        }
+
+        /// <summary>
+        /// タイマーが始まってからの総時間
+        /// </summary>
+        /// <returns></returns>
+        public float TotalTime()
+        {
+            return (float)sw.ElapsedMilliseconds / 1000.0f; 
+        }
+    }
+}
--- 2DActionGame/2DActionGame/Core/Scenes/Ending.cs
+++ 2DActionGame/2DActionGame/Core/Scenes/Ending.cs
@@ -13,12 +13,13 @@ namespace _2DActionGame
     public class Ending : Scene
     {
 		private const int textureNum = 9;
+        private Timer timer;
 		private readonly int defDrawTime;// = 810;// 6480 / 8 = 810
         private readonly int musicTime = 6480;
         //1:48 = 108 * 60 = 6480frame
         private Texture2D[] textures = new Texture2D[textureNum];
         private bool fromGame;
-		private float[] timings = { 0, 18.5f, 37, 47, 60, 75, 84, 93, 102, 120 };
+		private float[] timings = { 0, 18.5f, 37.4f, 46.7f, 60.5f, 74.75f, 84.25f, 93.5f, 104, 120 };
 		private void WriteScore()
 		{
 		}
@@ -29,6 +30,7 @@ namespace _2DActionGame
 			Load();
             defDrawTime = musicTime / textureNum;
             this.fromGame = fromGame;
+            this.timer = new Timer();
         }
 
         public override void Load() 
@@ -42,7 +44,10 @@ namespace _2DActionGame
         }
 		public override void Update(double dt) 
         {
-            if (counter > defDrawTime * textureNum || JoyStick.IsOnKeyDown(1)) {
+            if (!timer.IsStarted) {
+                timer.Start();
+            }
+            if (timer.TotalTime() > (float)timings.Last() || JoyStick.IsOnKeyDown(1)) {
 				SoundControl.Stop();
 				SoundControl.IniMusic("Audio\\BGM\\menu_new");
 				//BackScene(5);
@@ -51,13 +56,13 @@ namespace _2DActionGame
                     //BackScene(8);
                 } else isEndScene = true;
             }
-            counter++;
+            
         }
 		public override void Draw(SpriteBatch spriteBatch)
         {
 			if (textures[0] != null) {
                 for (int i = 0; i < textureNum; i++)
-					if (counter >= timings[i] * 60 && counter < timings[i + 1] * 60)
+					if (timer.TotalTime() >= timings[i] && timer.TotalTime() < timings[i + 1])
 					//if (counter >= defDrawTime * i && counter < defDrawTime * (i + 1))
 						spriteBatch.Draw(textures[i], Vector2.Zero, Color.White);
 			}
